00a2cf70b4e1830036d2f6916705c76f017b0635
rubi-ekak <125873059+rubi-ekak@users.noreply.github.com>
2023-03-04 13:04:30 +0530

    Add files via upload
    UI changes 4-03-2023 13:04

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -88,7 +88,7 @@ const Footer = () => {
           </div>
           <div className="col-lg-4 col-md-4 col-12 d-flex align-items-center justify-content-center ml-4">
             <div className="social-media ">
-              <NavLink to="/">
+              <NavLink path="/">
                 <img
                   src="https://ekak.in/assets/images/Ekak-12.png"
                   className="ekak"
